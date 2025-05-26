543fa1bdcf16ffb0e93e3abbf833257f14f566c3
Charalambos <charith12.CT@gmail.com>
2025-05-26 10:42:25 +0300

    Basic HTTP server that accepts connections and prints incoming request headers

---
+++ HttpServer.java
@@ -0,0 +1,24 @@
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class HttpServer {
+
+    public static void main(String[] args0) throws Exception {
+        final ServerSocket server = new ServerSocket(8080);
+        System.out.println("Listening for connection on port 8080....");
+        while (true) {
+            Socket client = server.accept();
+            InputStreamReader isr = new InputStreamReader(client.getInputStream());
+            BufferedReader reader = new BufferedReader(isr);
+            String line = reader.readLine();
+            while (!line.isEmpty()) {
+                System.out.println(line);
+                line = reader.readLine();
+            }
+        }
+    }
+
+}
